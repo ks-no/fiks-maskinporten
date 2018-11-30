7ddb00e89ade4de443e1f91ef25f35fa2eaf2da0
Andreas Berre <andreas.berre@webstep.no>
2018-11-30 07:00:12 +0100

    la til validering på props

--- fiks-maskinporten-spring-boot-client/src/main/java/no/ks/fiks/maskinporten/MaskinportenProperties.java
+++ fiks-maskinporten-spring-boot-client/src/main/java/no/ks/fiks/maskinporten/MaskinportenProperties.java
@@ -4,12 +4,14 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @ConfigurationProperties(prefix = "maskinporten")
 @Validated
 public class MaskinportenProperties {
-    private String audience;
-    private String tokenEndpoint;
-    private String issuer;
+    @NotNull private String audience;
+    @NotNull private String tokenEndpoint;
+    @NotNull private String issuer;
     private int numberOfSecondsLeftBeforeExpire;
 }
