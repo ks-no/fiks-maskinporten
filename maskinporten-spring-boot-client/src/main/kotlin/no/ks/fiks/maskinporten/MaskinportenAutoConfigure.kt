53aa588cd37df480e323adc2bf3909f0dad670b0
Sondre Eikanger Kvalø <zapodot@gmail.com>
2021-12-22 10:22:27 +0100

    Kotlinifisert autoconfiguration og lagt til tester

--- maskinporten-spring-boot-client/src/main/kotlin/no/ks/fiks/maskinporten/MaskinportenAutoConfigure.kt
+++ maskinporten-spring-boot-client/src/main/kotlin/no/ks/fiks/maskinporten/MaskinportenAutoConfigure.kt
@@ -1,30 +1,22 @@
-package no.ks.fiks.maskinporten;
+package no.ks.fiks.maskinporten
 
-import no.ks.fiks.virksomhetsertifikat.VirksomhetSertifikater;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateEncodingException;
+import no.ks.fiks.virksomhetsertifikat.VirksomhetSertifikater
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties({MaskinportenProperties.class})
-public class MaskinportenAutoConfigure {
+@EnableConfigurationProperties(MaskinportenProperties::class)
+class MaskinportenAutoConfigure {
 
+    @ConditionalOnBean(VirksomhetSertifikater::class)
+    @ConditionalOnMissingBean
     @Bean
-    public Maskinportenklient getMaskinportenklient(MaskinportenProperties properties, VirksomhetSertifikater virksomhetSertifikater) throws UnrecoverableKeyException, CertificateEncodingException, NoSuchAlgorithmException, KeyStoreException {
-        VirksomhetSertifikater.KsVirksomhetSertifikatStore authKeyStore = virksomhetSertifikater.requireAuthKeyStore();
-
-        return new Maskinportenklient(authKeyStore.getKeyStore(), authKeyStore.getPrivateKeyAlias(), authKeyStore.getPrivateKeyPassword(), MaskinportenklientProperties.builder()
-                .numberOfSecondsLeftBeforeExpire(properties.getNumberOfSecondsLeftBeforeExpire())
-                .issuer(properties.getIssuer())
-                .audience(properties.getAudience())
-                .tokenEndpoint(properties.getTokenEndpoint())
-                .consumerOrg(properties.getConsumerOrg())
-                .build());
+    fun getMaskinportenklient(properties: MaskinportenProperties, virksomhetSertifikater: VirksomhetSertifikater): Maskinportenklient {
+        val authKeyStore = virksomhetSertifikater.requireAuthKeyStore()
+        return Maskinportenklient(
+            authKeyStore.keyStore, authKeyStore.privateKeyAlias, authKeyStore.privateKeyPassword, properties.toMaskinportenklientProperties())
     }
 }
-
--- maskinporten-spring-boot-client/src/main/kotlin/no/ks/fiks/maskinporten/MaskinportenProperties.kt
+++ maskinporten-spring-boot-client/src/main/kotlin/no/ks/fiks/maskinporten/MaskinportenProperties.kt
@@ -1,18 +1,29 @@
-package no.ks.fiks.maskinporten;
+package no.ks.fiks.maskinporten
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.validation.annotation.Validated
+import javax.validation.constraints.NotNull
 
-import javax.validation.constraints.NotNull;
+internal const val DEFAULT_SECONDS_LEFT_BEFORE_EXPIRATION = 10
 
-@Data
+@ConstructorBinding
 @ConfigurationProperties(prefix = "maskinporten")
 @Validated
-public class MaskinportenProperties {
-    @NotNull private String audience;
-    @NotNull private String tokenEndpoint;
-    @NotNull private String issuer;
-    private String consumerOrg = null;
-    private int numberOfSecondsLeftBeforeExpire = 10;
+data class MaskinportenProperties(
+    val audience: @NotNull String? = null,
+    val tokenEndpoint: @NotNull String? = null,
+    val issuer: @NotNull String? = null,
+    val consumerOrg: String? = null,
+    val numberOfSecondsLeftBeforeExpire: Int = DEFAULT_SECONDS_LEFT_BEFORE_EXPIRATION
+) {
+    fun toMaskinportenklientProperties(): MaskinportenklientProperties = MaskinportenklientProperties.builder()
+        .numberOfSecondsLeftBeforeExpire(numberOfSecondsLeftBeforeExpire)
+        .issuer(issuer ?: throw IllegalArgumentException(""""issuer" property can not be null"""))
+        .audience(audience ?: throw IllegalArgumentException(""""audience" property can not be null"""))
+        .tokenEndpoint(tokenEndpoint?: throw IllegalArgumentException(""""tokenEndpoint" property can not be null"""))
+        .also {
+            consumerOrg?.run { it.consumerOrg(this) }
+        }
+        .build()
 }
