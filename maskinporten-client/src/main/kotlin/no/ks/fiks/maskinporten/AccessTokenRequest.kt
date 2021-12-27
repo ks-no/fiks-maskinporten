d84d93c93f84a6b015230c3ed43443ed1a7a2b62
Sondre Eikanger Kvalø <zapodot@gmail.com>
2021-12-27 09:48:04 +0100

    La på litt mer javadocs

--- maskinporten-client/src/main/kotlin/no/ks/fiks/maskinporten/AccessTokenRequest.kt
+++ maskinporten-client/src/main/kotlin/no/ks/fiks/maskinporten/AccessTokenRequest.kt
@@ -14,16 +14,22 @@ data class AccessTokenRequest(
     val consumerOrg: String? = null,
 
     /**
-     * Ønsket audience for access token. Optional.
+     * Ønsket audience for access token. Valgfritt.
      */
     val audience: String?) {
 
     companion object {
+        /**
+         * Oppretter en builder som skal gjøre det lettere å bygge AccessTokenRequest fra Java
+         */
         @JvmStatic
         fun builder() = AccessTokenRequestBuilder()
     }
 }
 
+/**
+ * Builder som gjør det lettere for klienter implementert i Java å bygge en AccessTokenRequest
+ */
 class AccessTokenRequestBuilder {
 
     private var scopes: Set<String> = emptySet()
@@ -32,28 +38,43 @@ class AccessTokenRequestBuilder {
 
     private var audience: String? = null
 
+    /**
+     * Legger til et scope som skal brukes i forespørsel mot Maskinporten. Minst et scope må oppgies
+     */
     fun scope(scope: String): AccessTokenRequestBuilder {
         this.scopes += scope
         return this
     }
 
-    fun scopes(scopes: Set<String>): AccessTokenRequestBuilder {
+    /**
+     * Legger til et set med scopes som skal brukes i forespørsel mot Maskinporten. Minst et scope må oppgies
+     */
+    fun scopes(scopes: java.util.Set<String>): AccessTokenRequestBuilder {
         this.scopes = scopes.toSet()
         return this
     }
 
+    /**
+     * Legger til verdi som skal brukes i "aud" feltet i forespørsel mot Maskinporten. Valgfritt
+     */
     fun audience(audience: String): AccessTokenRequestBuilder {
         this.audience = audience
         return this
     }
 
+    /**
+     * Brukes til forespørsler der man ønsker å få generert et token på vegne av en annen organisasjon.
+     */
     fun consumerOrg(consumerOrg: String): AccessTokenRequestBuilder {
         this.consumerOrg = consumerOrg
         return this
     }
 
+    /**
+     * Bygger forespørselsobjekt
+     */
     fun build(): AccessTokenRequest = AccessTokenRequest(
-        scopes = this.scopes.toSet(),
+        scopes = scopes.toSet(),
         audience = this.audience,
         consumerOrg = this.consumerOrg
     )
